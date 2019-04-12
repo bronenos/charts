8200deefc726325d88e745f4e973335a67234042
Stan Potemkin <bronenos@gmail.com>
2019-04-12 22:32:15 +0300

    Improve the timing generation

--- TGCharts/Controls/ChartControl/Nodes/Controls/ChartTimelineControl/ChartTimelineControl.swift
+++ TGCharts/Controls/ChartControl/Nodes/Controls/ChartTimelineControl/ChartTimelineControl.swift
@@ -16,7 +16,7 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
     private let chart: Chart
     private let formattingProvider: IFormattingProvider
     
-    private var dateNodes = [ChartLabelNode]()
+    private var dateNodes = [Int: ChartLabelNode]()
     
     private var config: ChartConfig
     private var overlap = UIEdgeInsets.zero
@@ -29,17 +29,6 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
         super.init(frame: .zero)
         
         clipsToBounds = true
-        
-        dateNodes = chart.axis.reversed().map { item in
-            let node = ChartLabelNode()
-            node.text = formattingProvider.format(date: item.date, style: .shortDate)
-            node.textColor = DesignBook.shared.color(.chartIndexForeground)
-            node.font = UIFont.systemFont(ofSize: 8)
-            node.textAlignment = .right
-            addSubview(node)
-            
-            return node
-        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -53,12 +42,12 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
     
     override func updateDesign() {
         super.updateDesign()
-        colorizeDateNodes(dateNodes)
+        colorizeDateNodes(Array(dateNodes.values))
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        resizeDateNodes(dateNodes)
+        resizeDateNodes(Array(dateNodes.values))
         update()
     }
     
@@ -66,7 +55,7 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
         let dateWidth = bounds.size.width / 7
         let height = bounds.size.height
         
-        dateNodes.forEach { node in
+        dateNodes.values.forEach { node in
             node.transform = .identity
             node.frame = CGRect(x: 0, y: 0, width: dateWidth, height: height)
         }
@@ -75,7 +64,7 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
     private func colorizeDateNodes(_ nodes: [ChartLabelNode]) {
         let color = DesignBook.shared.color(.chartIndexForeground)
         
-        dateNodes.forEach { node in
+        dateNodes.values.forEach { node in
             node.textColor = color
         }
     }
@@ -92,13 +81,37 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
         let dateWidth = bounds.width / 7
         let totalWidth = bounds.width / config.range.distance
         let rightEdge = bounds.width + (1.0 - config.range.end) * totalWidth
-
-        dateNodes.enumerated().reversed().forEach { index, node in
+        
+        let leftIndex = Int(floor(CGFloat(chart.axis.count) * config.range.start))
+        let rightIndex = Int(ceil(CGFloat(chart.axis.count) * config.range.end))
+        
+        (leftIndex ..< rightIndex).forEach { index in
+            let reversedIndex = chart.axis.count - index - 1
+            guard dateNodes[reversedIndex] == nil else { return }
+            
+            let item = chart.axis[index]
+            let node = ChartLabelNode()
+            
+            node.frame = CGRect(x: 0, y: 0, width: dateWidth, height: bounds.height)
+            node.text = formattingProvider.format(date: item.date, style: .shortDate)
+            node.textColor = DesignBook.shared.color(.chartIndexForeground)
+            node.font = UIFont.systemFont(ofSize: 8)
+            node.textAlignment = .right
+            
+            addSubview(node)
+            dateNodes[reversedIndex] = node
+        }
+        
+        dateNodes.forEach { index, node in
             let leftX = rightEdge - CGFloat(index) * meta.stepX - dateWidth
             node.transform = CGAffineTransform(translationX: leftX, y: 0)
         }
 
-        adjustLayout(dateWidth: dateWidth, rightEdge: rightEdge, meta: meta)
+        adjustLayout(
+            dateWidth: dateWidth,
+            rightEdge: rightEdge,
+            meta: meta
+        )
     }
     
     private func calculateStep(dateWidth: CGFloat, rightEdge: CGFloat, meta: ChartSliceMeta) -> Int {
@@ -109,18 +122,35 @@ final class ChartTimelineControl: ChartNode, IChartTimelineControl {
     private func adjustLayout(dateWidth: CGFloat, rightEdge: CGFloat, meta: ChartSliceMeta) {
         let skippingStep = calculateStep(dateWidth: dateWidth, rightEdge: rightEdge, meta: meta)
         let skippingSemiStep = max(1, skippingStep / 2)
-        var lastAnchor = CGFloat(0)
         
-        for (index, node) in dateNodes.enumerated() {
+        let fadingStartDistance = dateWidth * 0.1
+        let fadingEndDistance = dateWidth * 0.3
+
+        for (index, node) in dateNodes {
             if (index % skippingStep) == 0 {
                 node.alpha = 1.0
-                lastAnchor = node.frame.minX
             }
             else if (index % skippingSemiStep) == 0 {
-                let distance = node.frame.maxX - lastAnchor
-                let limit = node.frame.size.width * 0.25
-                let value = 1.0 - min(1.0, distance / limit)
-                node.alpha = value
+                let primaryIndex = index - skippingSemiStep
+                let primaryLeftAnchor = rightEdge - CGFloat(primaryIndex) * meta.stepX - dateWidth
+                let rightAnchor = rightEdge - CGFloat(index) * meta.stepX
+                
+                if primaryLeftAnchor < rightAnchor {
+                    let distance = rightAnchor - primaryLeftAnchor
+                    if distance < fadingStartDistance {
+                        node.alpha = 1.0
+                    }
+                    else if distance > fadingEndDistance {
+                        node.alpha = 0
+                    }
+                    else {
+                        let coef = distance.percent(from: fadingStartDistance, to: fadingEndDistance)
+                        node.alpha = 1.0 - coef
+                    }
+                }
+                else {
+                    node.alpha = 1.0
+                }
             }
             else {
                 node.alpha = 0
